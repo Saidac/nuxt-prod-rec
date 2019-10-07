a9a99d3ef3be893f3e6313ecfc4d32d1299b3748
William Saidac <william.saidac@gmail.com>
2019-10-07 23:10:05 +0200

    home page with product

--- pages/hp.vue
+++ pages/hp.vue
@@ -1,14 +1,26 @@
 <template>
-    <div>
-        <h1>
-            Home Page
-        </h1>
+    <div class="container mx-auto my-5 p-10">
+        <div class="bg-white border rounded-lg m-5 p-5 w-56">
+            <a :href='product.href'>
+                <img :alt='product.alt' :src='product.src'>            
+            </a>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data: function() {
+            return {
+                product:
+                {
+                    id: 1,
+                    href: '/en-gb/playstation-plus-membership',
+                    alt: 'PlayStation Plus',
+                    src: 'https://static.rapido.com/cms/sites/19/2019/07/16091920/1EPClSqCSzjVtsytQbeJNV/Playstation_Plus_Logo_Popular_Products.jpg'
+                }
+            }
+        }
     }
 </script>
 
